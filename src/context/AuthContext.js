6e372a3c69832c3990a53fbecfea35844a3f2d3f
bhavesh0206s <bhaveshsuthar0299@gmail.com>
2020-11-13 23:38:27 +0530

    updated react-script

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,14 +1,15 @@
 import React, { createContext } from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import fire from '../firebase'
+import fire, { fireDb } from '../firebase'
 
 export const AuthContext = createContext()
 
 const AuthContextProvider = (props) => {
   const [currentUser, setCurrentUser] = useState(null)
-  const [redirect, setRedirect] = useState(false)
-  const [isLoading, setLoading] = useState(false)
+  const [redirect, setRedirect] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState('')
   
   useEffect(()=>{
     fire.auth().onAuthStateChanged((user)=>{
@@ -25,22 +26,39 @@ const AuthContextProvider = (props) => {
 
   const signin = async (email,password) => {
     setLoading(true)
+    const userInfo = {email, password}
     await fire.auth().signInWithEmailAndPassword(email,password);
+    let userid =  fire.auth().currentUser.uid;
+    let userDoc = fireDb.collection('user-info').doc(`${userid}`);
+    userDoc.get().then(function(doc) {
+      if (!doc.exists) {
+        setError('No User Found')
+      } else {
+        let data = doc.data()
+        console.log(data);
+        // await fire.auth().signInWithEmailAndPassword(email,password);
+      }
+    }).catch(e => console.log(e))
     setLoading(false)
-    // Cookies.set('user', `${getUid()}`)
   }
 
   const signup = async (email, password, name) =>{
     try{
-      setLoading(true)
-      await fire.auth().createUserWithEmailAndPassword(email, password);
-      setLoading(false)
-      return fire.auth().currentUser.updateProfile({
-        displayName: name
+      let userDoc = fireDb.collection('user-info').doc('dsdasdas');
+      userDoc.get().then(async function(doc) {
+        if (!doc.exists) {
+          userDoc.set(email, password, name)
+          await fire.auth().createUserWithEmailAndPassword(email, password);
+        } else {
+          let data = doc.data()
+          console.log(data)
+        }
+        // setLoading(false)
       })
     }
     catch(e){
       console.log(e)
+      // setError(e)
     }
   }
 
